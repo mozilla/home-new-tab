c49d4248ba045deee2f8f78be6657a7c61b225fd
Joel <joel.kelly@me.com>
2026-01-09 10:51:46 -0800

    build(typescript): clean up one offs

--- clients/renderer/global.d.ts
+++ clients/renderer/global.d.ts
@@ -3,6 +3,11 @@ declare module "*.module.css" {
   export default classes
 }
 
+declare module "*.css" {
+  const css: string
+  export default css
+}
+
 declare module "*.svg?react" {
   import * as React from "react"
   const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
