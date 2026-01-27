7222a257e667d8af093d91c43d872dfdd0713b63
Joel <joel.kelly@me.com>
2026-01-27 00:16:10 -0800

    feat(gen): more robust state with connections

--- config/generator-config/data-state/_plop.ts
+++ config/generator-config/data-state/_plop.ts
@@ -11,11 +11,25 @@ export const statePlop = {
     },
   ],
   actions: [
+    // Core domain files
     {
       type: "addMany",
       skipIfExists: true,
       destination: "{{ turbo.paths.root }}/data/state/{{ stateName }}/",
-      templateFiles: ["data-state/index.ts.hbs"],
+      templateFiles: [
+        "data-state/index.ts.hbs",
+        "data-state/types.ts.hbs",
+      ],
+    },
+
+    // Optional React hook (UI-facing)
+    {
+      type: "add",
+      skipIfExists: true,
+      path: "{{ turbo.paths.root }}/ui/hooks/use{{pascalCase stateName}}Display.ts",
+      templateFile: "data-state/hook.ts.hbs",
+      skip: (data: { includeUiHook?: boolean }) =>
+        data.includeUiHook ? false : "Skipping UI hook",
     },
   ],
 }
