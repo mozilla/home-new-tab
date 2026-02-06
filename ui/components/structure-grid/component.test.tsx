c9dfc67bd93638528f4bc52f5f7b1a7910f8265b
Joel <joel.kelly@me.com>
2026-02-06 12:06:42 -0800

    chore(cleanup): remove outdated props

--- ui/components/structure-grid/component.test.tsx
+++ ui/components/structure-grid/component.test.tsx
@@ -2,13 +2,12 @@ import "@testing-library/jest-dom/vitest"
 import { render } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
 
-import { GridType } from "@common/types"
 import { Grid as Component } from "."
 
 describe("renders Grid", () => {
   it("with defaults", () => {
     const rendered = render(
-      <Component gridType={GridType.FLUID}>
+      <Component>
         <div></div>
       </Component>,
     )
