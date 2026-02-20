004b9306bf0c470252bb47b6eaffacf986575e61
Joel <joel.kelly@me.com>
2026-02-20 12:38:23 -0800

    refactor(stories): switch from complete to overview

--- ui/components/header/component.story.tsx
+++ ui/components/header/component.story.tsx
@@ -4,13 +4,13 @@ import type { Meta, StoryObj } from "@storybook/react-vite"
 
 // Storybook Meta
 const meta: Meta<typeof Component> = {
-  title: "Header / Complete",
+  title: "Header / Overview",
   component: Component,
 }
 export default meta
 
 // Stories
-export const Complete: StoryObj<typeof Component> = {
+export const Overview: StoryObj<typeof Component> = {
   render: () => {
     return <Component />
   },
--- ui/components/timer/component.story.tsx
+++ ui/components/timer/component.story.tsx
@@ -5,13 +5,13 @@ import type { Meta, StoryObj } from "@storybook/react-vite"
 
 // Storybook Meta
 const meta: Meta<typeof Component> = {
-  title: "Timer / Complete",
+  title: "Timer / Overview",
   component: Component,
 }
 export default meta
 
 // Stories
-export const Complete: StoryObj<typeof Component> = {
+export const Overview: StoryObj<typeof Component> = {
   render: () => {
     return <Component />
   },
