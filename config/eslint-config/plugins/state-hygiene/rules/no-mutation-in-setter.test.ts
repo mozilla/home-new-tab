39f1b67702a099d4a7bbb293b91093c901e3864b
Joel <joel.kelly@me.com>
2026-02-22 16:04:24 -0800

    build(eslint): make eslint play nice with ci

--- config/eslint-config/plugins/state-hygiene/rules/no-mutation-in-setter.test.ts
+++ config/eslint-config/plugins/state-hygiene/rules/no-mutation-in-setter.test.ts
@@ -1,6 +1,6 @@
 import { describe } from "vitest"
-import rule from "./no-mutation-in-setter.js"
-import { ruleTester } from "./rule-tester.js"
+import rule from "./no-mutation-in-setter.ts"
+import { ruleTester } from "./rule-tester.ts"
 
 describe("state-hygiene/no-mutation-in-setter (smoke)", () => {
   ruleTester.run("no-mutation-in-setter", rule as any, {
