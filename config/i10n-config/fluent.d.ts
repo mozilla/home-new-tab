ba852d20e96cf121897f379c5233b1f7010ed2e5
Joel <joel.kelly@me.com>
2026-03-09 10:04:48 -0700

    feat(l10n): common/config localization

---
+++ config/i10n-config/fluent.d.ts
@@ -0,0 +1,7 @@
+declare module "virtual:fluent/*" {
+  export const surface: string
+  export const locale: string
+  export const ftl: string
+  const _default: string
+  export default _default
+}
