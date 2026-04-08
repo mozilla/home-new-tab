907fb0bf9f709704fd9a8bafcbe9e98319d7d076
Joel <joel.kelly@me.com>
2026-04-08 09:42:07 -0700

    build(a11y): add linting

--- config/eslint-config/component.js
+++ config/eslint-config/component.js
@@ -1,4 +1,5 @@
 import baseConfig from "./base.js"
+import jsxA11y from "eslint-plugin-jsx-a11y"
 import stateHygiene from "./plugins/state-hygiene/index.ts"
 import noMissingMessage from "./plugins/fluent-l10n/index.ts"
 
@@ -15,9 +16,15 @@ export const componentConfig = [
     files: ["**/*.tsx"],
     plugins: {
       "fluent-l10n": noMissingMessage,
+      "jsx-a11y": jsxA11y,
     },
     rules: {
       "fluent-l10n/no-missing-message": "warn",
+      ...jsxA11y.configs.recommended.rules,
+      // Fluent injects heading content at runtime via data-l10n-id. The rule
+      // cannot see that and fires on every localized heading. The
+      // fluent-l10n/no-missing-message rule is the enforcement mechanism here.
+      "jsx-a11y/heading-has-content": "off",
     },
   },
 ]
